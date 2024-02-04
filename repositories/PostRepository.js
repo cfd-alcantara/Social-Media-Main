8f8f97769b9ae9b388a221d8260ab73ce298c378
jp-gerona <julianpetergerona@gmail.com>
2024-02-04 11:48:15 +0800

    More error validations + GET follower endpoint

--- repositories/PostRepository.js
+++ repositories/PostRepository.js
@@ -1,10 +1,11 @@
 import { nanoid } from "nanoid";
+import DataAccessError from "./DataAccessError.js";
 
-const posts = {};
+const postsPerUser = {};
 
 function createPost(username, post, dateTimePosted) {
-  if (posts[username] === undefined) {
-    posts[username] = [];
+  if (postsPerUser[username] === undefined) {
+    throw new DataAccessError("User does not exist");
   }
 
   const createdPost = {
@@ -15,49 +16,66 @@ function createPost(username, post, dateTimePosted) {
     likes: [],
   };
 
-  posts[username].push(createdPost);
+  postsPerUser[username].push(createdPost);
   return createdPost;
 }
 
 function getPostOfUser(username) {
-  if (posts[username] === undefined) {
-    return [];
+  if (postsPerUser[username] === undefined) {
+    throw new DataAccessError("User does not exist");
   }
-  return posts[username];
+  return postsPerUser[username];
 }
 
 function getFeedOfUser(username, followingUsernames) {
   const feed = [...getPostOfUser(username)];
-  console.log(followingUsernames);
   for (const username of followingUsernames) {
-    if (posts[username] !== undefined) {
-      feed.push(...posts[username]);
+    if (postsPerUser[username] !== undefined) {
+      feed.push(...postsPerUser[username]);
     }
   }
   return feed;
 }
 
-function likePost(username, postId) {
-  const post = posts[username].find((post) => post.postId === postId);
-  if (post === undefined) {
-    return null;
+function getPost(postId) {
+  for (const [_, posts] of Object.entries(postsPerUser)) {
+    for (const post of posts) {
+      const currentPostId = post.postId;
+      if (currentPostId === postId) {
+        return post;
+      }
+    }
   }
+  return undefined;
+}
 
+function likePost(username, postId) {
+  const post = getPost(postId);
+  if (post === undefined) {
+    throw new DataAccessError("Post does not exist");
+  }
   if (post.likes.includes(username)) {
     return post;
   }
   post.likes.push(username);
+  console.log(post);
   return post;
 }
 
 function unlikePost(username, postId) {
-  const post = posts[username].find((post) => post.postId === postId);
+  const post = getPost(postId);
   if (post === undefined) {
-    return null;
+    throw new DataAccessError("Post does not exist");
   }
-
+  if (!post.likes.includes(username)) {
+    return post;
+  }
   post.likes = post.likes.filter((like) => like !== username);
   return post;
+}
+
+function initializePosts(username) {
+  postsPerUser[username] = [];
 }
 
 export default {
@@ -66,4 +84,5 @@ export default {
   getFeedOfUser,
   likePost,
   unlikePost,
+  initializePosts,
 };
