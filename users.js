ea5e4a42d66711ef79aa4c712c0918d960b5fae3
Carl Alcantara <carlfrancis.alcantara@gmail.com>
2024-02-09 23:04:17 +0800

    Added a fetch api for the login page

--- users.js
+++ users.js
@@ -18,3 +18,25 @@ async function postCreateUser() {
         console.log("registration fails")
     }
 }
+
+// async function postLogIn() {
+//     var username = document.getElementById('Username').value;
+//     var password = document.getElementById('Password').value;
+
+//     res = await fetch("http://localhost:3000/api/v1/auth/register", {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             "username": username,
+//             "password": password
+//         })
+//     })
+//     if(res.ok){
+//         console.log(res)
+//         console.log("successful")
+//     } else {
+//         console.log("login fails")
+//     }
+// }
